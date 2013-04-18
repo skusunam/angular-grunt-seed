369cc72497031f73d213442ed9b99120b088039b
Srinivas Kusunam <skusunam@libertyleap.net>
2013-04-18 16:02:54 -0500

    2 spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
 
-	  pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON('package.json'),
 	
     //JS Source files
     src: {
