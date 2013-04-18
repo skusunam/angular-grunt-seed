b1b0ea587230818f36617e1ca502270b9a3c6e08
Srinivas Kusunam <skusunam@libertyleap.net>
2013-04-18 16:02:10 -0500

    format to 2 spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,42 +2,41 @@
 
 module.exports = function(grunt) {
 
-	grunt.initConfig({
+  grunt.initConfig({
 
-		pkg: grunt.file.readJSON('package.json'),
+	  pkg: grunt.file.readJSON('package.json'),
 	
-		//JS Source files
-		src: {
-			js: ['app/js/**/*.js']
-		},
+    //JS Source files
+    src: {
+      js: ['app/js/**/*.js']
+    },
 		
-		//JS Test files
-		test: {
-			karmaConfig: 'test/karma.conf.js',
-			unit: ['test/unit/**/*.js']
-		},
+    //JS Test files
+    test: {
+      karmaConfig: 'test/karma.conf.js',
+      unit: ['test/unit/**/*.js']
+    },
 	
-		// Configure Lint\JSHint Task
-		jshint: {
-			options: {
-				jshintrc: '.jshintrc'
-			},
-			all: [
-				'Gruntfile.js',
-				'<%= src.js %>',
-				'<%= test.unit %>'
-			]
-		},
+    // Configure Lint\JSHint Task
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: [
+        'Gruntfile.js',
+        '<%= src.js %>',
+        '<%= test.unit %>'
+      ]
+    },
 	
-		karma: {
-			unit: {
-				configFile: '<%= test.karmaConfig %>',
-				singleRun: true	
-			}
-		}
-	});
+    karma: {
+      unit: {
+        configFile: '<%= test.karmaConfig %>',
+        singleRun: true	
+      }
+    }
+  });
 
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-karma');
 };
-
